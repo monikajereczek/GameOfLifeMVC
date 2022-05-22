4c84819e7b73d714249a8bb3d35c7c50c943909e
M J <monikajereczek9@gmail.com>
2022-05-22 17:44:43 +0200

    class

--- GameOfLifeMVC/controller.py
+++ GameOfLifeMVC/controller.py
@@ -4,28 +4,32 @@ import numpy as np
 import pygame
 import models
 
-
-
-def game_loop(screen, bc_color, dead_color, alive_color):
-    """Game's loop"""
-    cells = np.zeros((60,75))
-    running = False
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key==pygame.K_SPACE:
-                    running=not running
-                    models.print_cells(screen, cells, 10, bc_color, alive_color)
+class GameController:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Game of Life')
+        self.screen= pygame.display.set_mode((750,600))
+        self.screen.fill((10,10,10))
+        pygame.display.update()
+        self.cells=models.Cells(60,75,10,(10,10,10),(0, 0, 250))
+        self.running=False
+    def game_loop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.KEYDOWN:
+                    if event.key==pygame.K_SPACE:
+                        self.running=not self.running
+                        self.cells.print_cells(self.screen)
+                        pygame.display.update()
+                if pygame.mouse.get_pressed()[0]:
+                    pos = pygame.mouse.get_pos()
+                    self.cells.cells[pos[1]//10 , pos[0]//10]=1
+                    self.cells.print_cells(self.screen)
                     pygame.display.update()
-            if pygame.mouse.get_pressed()[0]:
-                pos = pygame.mouse.get_pos()
-                cells[pos[1]//10 , pos[0]//10]=1
-                models.print_cells(screen, cells, 10, bc_color, alive_color)
+            if self.running:
+                self.cells.update_and_print(self.screen)
                 pygame.display.update()
-        if running:
-            cells = models.update_and_print(screen, cells, 10 ,bc_color, dead_color, alive_color)
-            pygame.display.update()
-            time.sleep(0.001)
+                time.sleep(0.001)
--- GameOfLifeMVC/models.py
+++ GameOfLifeMVC/models.py
@@ -2,31 +2,32 @@
 import pygame
 import numpy as np
 
+class Cells:
+    def __init__(self, x, y, size, bc_color, alive_color):
+        self.cells=np.zeros((x,y))
+        self.size=size
+        self.bc_color=bc_color
+        self.alive_color=alive_color
+    def print_cells(self, screen):
+        for row, col in np.ndindex(self.cells.shape):
+            if self.cells[row, col]==1:
+                pygame.draw.rect(screen, self.alive_color,(col*self.size, row*self.size, self.size-1, self.size-1),border_radius=2)
+            else:
+                pygame.draw.rect(screen, self.bc_color, (col*self.size, row*self.size, self.size-1, self.size-1),border_radius=2)
+    def update_and_print(self, screen):
+        updated_cells = np.zeros((self.cells.shape[0], self.cells.shape[1]))
+        for row, col in np.ndindex(self.cells.shape):
+            alive=np.sum(self.cells[row-1:row+2, col-1:col+2]) - self.cells[row,col]
+            color= self.bc_color if self.cells[row, col] == 0 else self.alive_color
+            if self.cells[row, col]==1:
+                if 2 <=alive <= 3:
+                    updated_cells[row, col]=1
+                    color = self.alive_color
+            else:
+                if alive==3:
+                    updated_cells[row, col]=1
+                    color = self.alive_color
+            pygame.draw.rect(screen, color, (col*self.size, row*self.size, self.size-1, self.size-1),border_radius=2)
+        self.cells=updated_cells
+       
 
-def print_cells(screen, cells, size, bc_color, alive_color ):
-    """Printing cells without calculations"""
-    for row, col in np.ndindex(cells.shape):
-        if cells[row, col]==1:
-            pygame.draw.rect(screen, alive_color,
-                             (col*size, row*size, size-1, size-1),border_radius=2)
-        else:
-            pygame.draw.rect(screen, bc_color, (col*size, row*size, size-1, size-1),border_radius=2)
-
-def update_and_print(screen, cells, size, bc_color, dead_color, alive_color):
-    """Printing cells with calculations"""
-    updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
-    for row, col in np.ndindex(cells.shape):
-        alive=np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row,col]
-        color= bc_color if cells[row, col] == 0 else alive_color
-        if cells[row, col]==1:
-            #if alive <2 or alive >3:
-            #    color= dead_color
-            if 2 <=alive <= 3:
-                updated_cells[row, col]=1
-                color = alive_color
-        else:
-            if alive==3:
-                updated_cells[row, col]=1
-                color = alive_color
-        pygame.draw.rect(screen, color, (col*size, row*size, size-1, size-1),border_radius=2)
-    return updated_cells
